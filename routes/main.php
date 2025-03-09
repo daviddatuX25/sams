8e01b052a25b2f1de3b1d7f9d70e64f3d4434857
sarmientofam-pc <David Datu PC>
2025-03-09 23:43:38 +0800

    CHANGE

--- routes/main.php
+++ routes/main.php
@@ -4,5 +4,10 @@ $routes = [
     "" => "Controller_Home@index",
     "home" => "Controller_Home@index",
     "home/index" => "Controller_Home@index",
+
+    "home/project" => "Controller_Home@project",
+    "home/creators" => "Controller_Home@creators",
+    "home/login" => "Controller_Home@login",
+    "home/register" => "Controller_Home@register"
 ];
 ?>
