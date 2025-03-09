cfc66ea1c1293571841579e68026ee15ea5cad0b
David Datu <youremail@example.com>
2025-03-09 10:04:27 +0800

    change samsdb_sql

---
+++ sams_db.sql
@@ -0,0 +1,398 @@
+-- phpMyAdmin SQL Dump
+-- version 5.2.1
+-- https://www.phpmyadmin.net/
+--
+-- Host: 127.0.0.1
+-- Generation Time: Mar 09, 2025 at 03:02 AM
+-- Server version: 10.4.32-MariaDB
+-- PHP Version: 8.2.12
+
+SET SQL_MODE = "NO_AUTO_VALUE_ON_ZERO";
+START TRANSACTION;
+SET time_zone = "+00:00";
+
+
+/*!40101 SET @OLD_CHARACTER_SET_CLIENT=@@CHARACTER_SET_CLIENT */;
+/*!40101 SET @OLD_CHARACTER_SET_RESULTS=@@CHARACTER_SET_RESULTS */;
+/*!40101 SET @OLD_COLLATION_CONNECTION=@@COLLATION_CONNECTION */;
+/*!40101 SET NAMES utf8mb4 */;
+
+--
+-- Database: `sams_db`
+--
+
+-- --------------------------------------------------------
+
+--
+-- Table structure for table `attendance`
+--
+
+CREATE TABLE `attendance` (
+  `attendance_id` int(10) UNSIGNED NOT NULL,
+  `user_id` int(10) UNSIGNED NOT NULL,
+  `class_session_id` int(10) UNSIGNED NOT NULL,
+  `time_in` datetime NOT NULL,
+  `time_out` datetime DEFAULT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_general_ci;
+
+-- --------------------------------------------------------
+
+--
+-- Table structure for table `attendance_leave`
+--
+
+CREATE TABLE `attendance_leave` (
+  `attendance_leave_id` int(10) UNSIGNED NOT NULL,
+  `user_id` int(10) UNSIGNED NOT NULL,
+  `status` enum('pending','approved','rejected') NOT NULL DEFAULT 'pending',
+  `letter` text NOT NULL,
+  `datetimestamp_created` datetime NOT NULL DEFAULT current_timestamp(),
+  `datetimestamp_reviewed` datetime DEFAULT NULL,
+  `datetimestamp_resolved` datetime DEFAULT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_general_ci;
+
+-- --------------------------------------------------------
+
+--
+-- Table structure for table `classes`
+--
+
+CREATE TABLE `classes` (
+  `class_id` int(10) UNSIGNED NOT NULL,
+  `subject_id` int(10) UNSIGNED NOT NULL,
+  `teacher_id` int(10) UNSIGNED NOT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_general_ci;
+
+-- --------------------------------------------------------
+
+--
+-- Table structure for table `class_room_time_slots`
+--
+
+CREATE TABLE `class_room_time_slots` (
+  `rts_id` int(10) UNSIGNED NOT NULL,
+  `room_id` int(10) UNSIGNED NOT NULL,
+  `time_start` time NOT NULL,
+  `time_end` time NOT NULL,
+  `week_day` enum('mon','tue','wed','thu','fri','sat') NOT NULL,
+  `class_id` int(10) UNSIGNED NOT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_general_ci;
+
+-- --------------------------------------------------------
+
+--
+-- Table structure for table `class_sessions`
+--
+
+CREATE TABLE `class_sessions` (
+  `class_session_id` int(10) UNSIGNED NOT NULL,
+  `class_id` int(10) UNSIGNED NOT NULL,
+  `open_datetime` datetime NOT NULL,
+  `close_datetime` datetime NOT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_general_ci;
+
+-- --------------------------------------------------------
+
+--
+-- Table structure for table `enrollments`
+--
+
+CREATE TABLE `enrollments` (
+  `enrollment_id` int(10) UNSIGNED NOT NULL,
+  `user_id` int(10) UNSIGNED NOT NULL,
+  `class_id` int(10) UNSIGNED NOT NULL,
+  `enrolled_datetime` datetime NOT NULL DEFAULT current_timestamp(),
+  `enrollment_term_id` int(10) UNSIGNED NOT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_general_ci;
+
+-- --------------------------------------------------------
+
+--
+-- Table structure for table `enrollment_term`
+--
+
+CREATE TABLE `enrollment_term` (
+  `enrollment_term_id` int(10) UNSIGNED NOT NULL,
+  `academic_year` varchar(9) NOT NULL,
+  `semester` enum('1st','2nd','summer') NOT NULL,
+  `sem_start` date NOT NULL,
+  `sem_end` date NOT NULL,
+  `term_start` date NOT NULL,
+  `term_end` date NOT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_general_ci;
+
+--
+-- Triggers `enrollment_term`
+--
+DELIMITER $$
+CREATE TRIGGER `before_insert_enrollment_term` BEFORE INSERT ON `enrollment_term` FOR EACH ROW BEGIN
+    -- Ensure sem_start is before or equal to sem_end
+    IF NEW.sem_start > NEW.sem_end THEN
+        SIGNAL SQLSTATE '45000' 
+        SET MESSAGE_TEXT = 'Semester start date must be before semester end date';
+    END IF;
+
+    -- Ensure term_start is before or equal to term_end
+    IF NEW.term_start > NEW.term_end THEN
+        SIGNAL SQLSTATE '45000' 
+        SET MESSAGE_TEXT = 'Term start date must be before term end date';
+    END IF;
+END
+$$
+DELIMITER ;
+DELIMITER $$
+CREATE TRIGGER `before_update_enrollment_term` BEFORE UPDATE ON `enrollment_term` FOR EACH ROW BEGIN
+    -- Ensure sem_start is before or equal to sem_end
+    IF NEW.sem_start > NEW.sem_end THEN
+        SIGNAL SQLSTATE '45000' 
+        SET MESSAGE_TEXT = 'Semester start date must be before semester end date';
+    END IF;
+
+    -- Ensure term_start is before or equal to term_end
+    IF NEW.term_start > NEW.term_end THEN
+        SIGNAL SQLSTATE '45000' 
+        SET MESSAGE_TEXT = 'Term start date must be before term end date';
+    END IF;
+END
+$$
+DELIMITER ;
+
+-- --------------------------------------------------------
+
+--
+-- Table structure for table `notifications`
+--
+
+CREATE TABLE `notifications` (
+  `notif_id` int(10) UNSIGNED NOT NULL,
+  `user_id` int(10) UNSIGNED NOT NULL,
+  `message` text NOT NULL,
+  `status` enum('read','unread') NOT NULL DEFAULT 'unread'
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_general_ci;
+
+-- --------------------------------------------------------
+
+--
+-- Table structure for table `rooms`
+--
+
+CREATE TABLE `rooms` (
+  `room_id` int(10) UNSIGNED NOT NULL,
+  `room_name` varchar(255) NOT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_general_ci;
+
+-- --------------------------------------------------------
+
+--
+-- Table structure for table `users`
+--
+
+CREATE TABLE `users` (
+  `user_id` int(10) UNSIGNED NOT NULL,
+  `first_name` varchar(255) NOT NULL,
+  `birthday` date DEFAULT NULL,
+  `password_hash` varchar(255) NOT NULL,
+  `role` enum('student','teacher','admin') NOT NULL,
+  `status` enum('active','inactive') NOT NULL DEFAULT 'active',
+  `gender` varchar(50) NOT NULL,
+  `last_name` varchar(255) NOT NULL,
+  `middle_name` varchar(255) NOT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_general_ci;
+
+--
+-- Indexes for dumped tables
+--
+
+--
+-- Indexes for table `attendance`
+--
+ALTER TABLE `attendance`
+  ADD PRIMARY KEY (`attendance_id`),
+  ADD KEY `idx_attendance_user` (`user_id`),
+  ADD KEY `idx_attendance_session` (`class_session_id`);
+
+--
+-- Indexes for table `attendance_leave`
+--
+ALTER TABLE `attendance_leave`
+  ADD PRIMARY KEY (`attendance_leave_id`),
+  ADD KEY `idx_leave_user` (`user_id`),
+  ADD KEY `idx_leave_status` (`status`);
+
+--
+-- Indexes for table `classes`
+--
+ALTER TABLE `classes`
+  ADD PRIMARY KEY (`class_id`),
+  ADD KEY `teacher_id` (`teacher_id`);
+
+--
+-- Indexes for table `class_room_time_slots`
+--
+ALTER TABLE `class_room_time_slots`
+  ADD PRIMARY KEY (`rts_id`),
+  ADD KEY `class_id` (`class_id`),
+  ADD KEY `idx_room_time` (`room_id`,`week_day`,`time_start`,`time_end`);
+
+--
+-- Indexes for table `class_sessions`
+--
+ALTER TABLE `class_sessions`
+  ADD PRIMARY KEY (`class_session_id`),
+  ADD KEY `class_id` (`class_id`);
+
+--
+-- Indexes for table `enrollments`
+--
+ALTER TABLE `enrollments`
+  ADD PRIMARY KEY (`enrollment_id`),
+  ADD UNIQUE KEY `user_id` (`user_id`,`class_id`),
+  ADD KEY `idx_enrollment_user` (`user_id`),
+  ADD KEY `idx_enrollment_class` (`class_id`),
+  ADD KEY `enrollment_term_id` (`enrollment_term_id`);
+
+--
+-- Indexes for table `enrollment_term`
+--
+ALTER TABLE `enrollment_term`
+  ADD PRIMARY KEY (`enrollment_term_id`);
+
+--
+-- Indexes for table `notifications`
+--
+ALTER TABLE `notifications`
+  ADD PRIMARY KEY (`notif_id`),
+  ADD KEY `idx_notif_user` (`user_id`),
+  ADD KEY `idx_notif_status` (`status`);
+
+--
+-- Indexes for table `rooms`
+--
+ALTER TABLE `rooms`
+  ADD PRIMARY KEY (`room_id`),
+  ADD UNIQUE KEY `room_name` (`room_name`);
+
+--
+-- Indexes for table `users`
+--
+ALTER TABLE `users`
+  ADD PRIMARY KEY (`user_id`);
+
+--
+-- AUTO_INCREMENT for dumped tables
+--
+
+--
+-- AUTO_INCREMENT for table `attendance`
+--
+ALTER TABLE `attendance`
+  MODIFY `attendance_id` int(10) UNSIGNED NOT NULL AUTO_INCREMENT;
+
+--
+-- AUTO_INCREMENT for table `attendance_leave`
+--
+ALTER TABLE `attendance_leave`
+  MODIFY `attendance_leave_id` int(10) UNSIGNED NOT NULL AUTO_INCREMENT;
+
+--
+-- AUTO_INCREMENT for table `classes`
+--
+ALTER TABLE `classes`
+  MODIFY `class_id` int(10) UNSIGNED NOT NULL AUTO_INCREMENT;
+
+--
+-- AUTO_INCREMENT for table `class_room_time_slots`
+--
+ALTER TABLE `class_room_time_slots`
+  MODIFY `rts_id` int(10) UNSIGNED NOT NULL AUTO_INCREMENT;
+
+--
+-- AUTO_INCREMENT for table `class_sessions`
+--
+ALTER TABLE `class_sessions`
+  MODIFY `class_session_id` int(10) UNSIGNED NOT NULL AUTO_INCREMENT;
+
+--
+-- AUTO_INCREMENT for table `enrollments`
+--
+ALTER TABLE `enrollments`
+  MODIFY `enrollment_id` int(10) UNSIGNED NOT NULL AUTO_INCREMENT;
+
+--
+-- AUTO_INCREMENT for table `enrollment_term`
+--
+ALTER TABLE `enrollment_term`
+  MODIFY `enrollment_term_id` int(10) UNSIGNED NOT NULL AUTO_INCREMENT;
+
+--
+-- AUTO_INCREMENT for table `notifications`
+--
+ALTER TABLE `notifications`
+  MODIFY `notif_id` int(10) UNSIGNED NOT NULL AUTO_INCREMENT;
+
+--
+-- AUTO_INCREMENT for table `rooms`
+--
+ALTER TABLE `rooms`
+  MODIFY `room_id` int(10) UNSIGNED NOT NULL AUTO_INCREMENT;
+
+--
+-- AUTO_INCREMENT for table `users`
+--
+ALTER TABLE `users`
+  MODIFY `user_id` int(10) UNSIGNED NOT NULL AUTO_INCREMENT;
+
+--
+-- Constraints for dumped tables
+--
+
+--
+-- Constraints for table `attendance`
+--
+ALTER TABLE `attendance`
+  ADD CONSTRAINT `attendance_ibfk_1` FOREIGN KEY (`user_id`) REFERENCES `users` (`user_id`) ON DELETE CASCADE,
+  ADD CONSTRAINT `attendance_ibfk_2` FOREIGN KEY (`class_session_id`) REFERENCES `class_sessions` (`class_session_id`) ON DELETE CASCADE;
+
+--
+-- Constraints for table `attendance_leave`
+--
+ALTER TABLE `attendance_leave`
+  ADD CONSTRAINT `attendance_leave_ibfk_1` FOREIGN KEY (`user_id`) REFERENCES `users` (`user_id`) ON DELETE CASCADE;
+
+--
+-- Constraints for table `classes`
+--
+ALTER TABLE `classes`
+  ADD CONSTRAINT `classes_ibfk_1` FOREIGN KEY (`teacher_id`) REFERENCES `users` (`user_id`) ON DELETE CASCADE;
+
+--
+-- Constraints for table `class_room_time_slots`
+--
+ALTER TABLE `class_room_time_slots`
+  ADD CONSTRAINT `class_room_time_slots_ibfk_1` FOREIGN KEY (`room_id`) REFERENCES `rooms` (`room_id`) ON DELETE CASCADE,
+  ADD CONSTRAINT `class_room_time_slots_ibfk_2` FOREIGN KEY (`class_id`) REFERENCES `classes` (`class_id`) ON DELETE CASCADE;
+
+--
+-- Constraints for table `class_sessions`
+--
+ALTER TABLE `class_sessions`
+  ADD CONSTRAINT `class_sessions_ibfk_1` FOREIGN KEY (`class_id`) REFERENCES `classes` (`class_id`) ON DELETE CASCADE;
+
+--
+-- Constraints for table `enrollments`
+--
+ALTER TABLE `enrollments`
+  ADD CONSTRAINT `enrollments_ibfk_1` FOREIGN KEY (`user_id`) REFERENCES `users` (`user_id`) ON DELETE CASCADE,
+  ADD CONSTRAINT `enrollments_ibfk_2` FOREIGN KEY (`class_id`) REFERENCES `classes` (`class_id`) ON DELETE CASCADE,
+  ADD CONSTRAINT `enrollments_ibfk_3` FOREIGN KEY (`enrollment_term_id`) REFERENCES `enrollment_term` (`enrollment_term_id`) ON DELETE CASCADE;
+
+--
+-- Constraints for table `notifications`
+--
+ALTER TABLE `notifications`
+  ADD CONSTRAINT `notifications_ibfk_1` FOREIGN KEY (`user_id`) REFERENCES `users` (`user_id`) ON DELETE CASCADE;
+COMMIT;
+
+/*!40101 SET CHARACTER_SET_CLIENT=@OLD_CHARACTER_SET_CLIENT */;
+/*!40101 SET CHARACTER_SET_RESULTS=@OLD_CHARACTER_SET_RESULTS */;
+/*!40101 SET COLLATION_CONNECTION=@OLD_COLLATION_CONNECTION */;
